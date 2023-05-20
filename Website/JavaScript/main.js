ac467fd37c8b3c15b7e2f39019d02551680d41f2
Amir Bekmetov <lebido_on@mail.ru>
2023-05-20 16:33:51 +0500

    The base
    I just added some documents, for beggining

---
+++ Website/JavaScript/main.js
@@ -0,0 +1,8 @@
+let codingButton = document.getElementById(codingButton);
+let expirience = document.getElementById(expirience);
+let clicksScore = 0;
+
+codingButton.addEventListener('click', function() {
+    clicksScore++;
+    expirience.textContent = 'experience:' + clicksScore;
+});
