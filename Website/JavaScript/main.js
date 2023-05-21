13b415987319b3664f88e9afca21eeaada64a322
Maggich <mvahaevich@mail.ru>
2023-05-21 21:39:56 +0500

    kosaki
    Debug

--- Website/JavaScript/main.js
+++ Website/JavaScript/main.js
@@ -2,7 +2,7 @@ let codingButton = document.getElementById('codingButton');
 let expirienceScore = document.getElementById('experienceScore')
 let clicksScore = 0;
 
-document.getElementById('codingButton').onclick = function() {
+codingButton.addEventListener('click', function() {
     clicksScore++;
-    document.querySelector('.experienceScore').innerHTML = clicksScore;
-}
+    expirienceScore.textContent="Click:"+clicksScore;
+})
