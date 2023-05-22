8440a8c2d791ec49fab8e60f4f5e24f952416ea0
Maggich <mvahaevich@mail.ru>
2023-05-23 00:21:03 +0500

    flexbox + photo

--- Website/JavaScript/main.js
+++ Website/JavaScript/main.js
@@ -4,5 +4,14 @@ let clicksScore = 0;
 
 codingButton.addEventListener('click', function() {
     clicksScore++;
-    expirienceScore.textContent = "experience:" + clicksScore;
+    expirienceScore.textContent="experience:"+clicksScore;
 })
+function myImg() {
+  let elem = document.getElementById('img_1'); 
+  let style = getComputedStyle(elem); 
+  if (style.display === 'none') {
+    document.getElementById('img_1').style.display='block';
+  } else if (style.display !== 'none') {
+    document.getElementById('img_1').style.display='none';
+  }
+}
