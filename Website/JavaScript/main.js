b59fd04465060a351440611bd3e97f3d20f08d0a
Amir Bekmetov <lebido_on@mail.ru>
2023-05-23 14:47:41 +0500

    Conventor
    I just tryed add conventor but i have some trouble

--- Website/JavaScript/main.js
+++ Website/JavaScript/main.js
@@ -1,10 +1,18 @@
 let codingButton = document.getElementById('codingButton');
 let expirienceScore = document.getElementById('experienceScore')
+let conventor = document.getElementById('conventor');
+let soldClick = document.getElementById('money')
 let clicksScore = 0;
+let money = 0;
 
 codingButton.addEventListener('click', function() {
     clicksScore++;
     expirienceScore.textContent="experience:"+clicksScore;
+})
+conventor.addEventListener('click', function() {
+  money = money + clicksScore;
+  clicksScore = clicksScore - clicksScore;
+  money.textContent="money:"+money;
 })
 function myImg() {
   let elem = document.getElementById('img_1'); 
